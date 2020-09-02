8ae065052eefe9e7e54c642d31c076ea7ee18885
VladPopa616 <61334330+VladPopa616@users.noreply.github.com>
2020-09-02 15:39:53 -0400

    Add files via upload

---
+++ StringBuilderDemo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.AppendLine("This is 2nd line");
+            sb.Append(".");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
